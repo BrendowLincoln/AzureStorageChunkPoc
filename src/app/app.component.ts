fed939db2b197e1cc57b8c187f1ec28643c3f2d2
Vanessa Oliveira <vanessa.oliveira@sapiensia.com>
2023-03-20 17:30:42 -0300

    Adicionando barra de progresso ao upload do arquivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,20 +15,22 @@ import { environment } from 'src/environments/environment';
 export class AppComponent {
 
   public currentFile: File | null = null;
+  public fileSize!: number;
+  public currentFileSize: number = 0;
 
   constructor(private http: HttpClient) {}
-  
-  
+
+
   public onFileChange = (event: any) => {
       this.currentFile = event.target.files[0];
       this.update();
   }
 
   public update = async () => {
+      this.fileSize = this.currentFile!.size;
 
+      var sasUri = "https://127.0.0.1:10000/devstoreaccount1/dmstemp/c8761f54-d564-4910-9894-c507e1510ee0?sv=2021-10-04&ss=btqf&srt=sco&st=2023-03-20T14%3A29%3A57Z&se=2023-03-21T14%3A29%3A57Z&sp=rwdflacu&sig=UM%2BVScWwyn5%2FXTgAkflm3dft6Tn3wOkU4DLiRd11x70%3D";
 
-      var sasUri = "https://127.0.0.1:10000/devstoreaccount1?sv=2021-10-04&ss=btqf&srt=sco&st=2023-03-20T14%3A29%3A57Z&se=2023-03-21T14%3A29%3A57Z&sp=rwdflacu&sig=UM%2BVScWwyn5%2FXTgAkflm3dft6Tn3wOkU4DLiRd11x70%3D";
-                
       var tempUri = sasUri.split("?", 2)[0];
       var splitted = tempUri.split("/");
       var containerName = splitted[splitted.length-2];
@@ -38,16 +40,19 @@ export class AppComponent {
       const blobServiceClient = new BlobServiceClient(sasUri)
       const containerClient = blobServiceClient.getContainerClient(containerName)
       const client = containerClient.getBlockBlobClient(filedId)
-      const response = await client.uploadData(this.currentFile!, { 
-        blockSize: 3 * 1024 * 1024 * 1024, // 3GB block size
+      const response = await client.uploadData(this.currentFile!, {
+        blockSize: 3 * 1024 * 1024, // 3GB block size
         concurrency: 20, // 20 concurrency
-        onProgress: (ev) => console.log(ev.loadedBytes),
-        blobHTTPHeaders :{ 
+        onProgress: (progress: any) => {
+          this.currentFileSize = progress.loadedBytes;
+          console.log(progress.loadedBytes);
+        },
+        blobHTTPHeaders :{
           blobContentType: 'application/octet-stream',
           blobContentDisposition: 'form-data'
         }
       })
 
       console.log(response._response.status)
-    } 
+    }
 }
