5cdbc828c301c85bd3352bcd558609f879611c2f
Brendow Souza <brendow.souza@sapiensia.com>
2023-03-20 16:19:02 -0300

    Trabalhando a string url.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import {AnonymousCredential, BlobServiceClient, newPipeline } from '@azure/storage-blob';
+import { BlobServiceClient } from '@azure/storage-blob';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { UploadUriInfo } from './dms.models';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -25,21 +21,25 @@ export class AppComponent {
 
   public update = async () => {
 
-      var sasUri = "https://127.0.0.1:10000/devstoreaccount1?sv=2021-10-04&ss=btqf&srt=sco&st=2023-03-20T14%3A29%3A57Z&se=2023-03-21T14%3A29%3A57Z&sp=rwdflacu&sig=UM%2BVScWwyn5%2FXTgAkflm3dft6Tn3wOkU4DLiRd11x70%3D";
-    
-      const blobServiceClient = new BlobServiceClient(sasUri)
-      const containerClient = blobServiceClient.getContainerClient("dmstemp")
-      if(!containerClient.exists()){
-      console.log("the container does not exit")
-      await containerClient.create()
+      var sasUri = "https://127.0.0.1:10000/devstoreaccount1/dmstemp/c8761f54-d564-4910-9894-c507e1510ee0?sv=2021-10-04&ss=btqf&srt=sco&st=2023-03-20T14%3A29%3A57Z&se=2023-03-21T14%3A29%3A57Z&sp=rwdflacu&sig=UM%2BVScWwyn5%2FXTgAkflm3dft6Tn3wOkU4DLiRd11x70%3D";
+  
+      var tempUri = sasUri.split("?", 2)[0];
+      var splitted = tempUri.split("/");
+      var containerName = splitted[splitted.length-2];
+      var filedId = splitted[splitted.length-1];
+      sasUri = sasUri.replace(`/${containerName}/`, "").replace(filedId, "");
 
-      }
-      const client = containerClient.getBlockBlobClient(this.currentFile!.name)
-      const response = await client.uploadData(this.currentFile!, {
+      const blobServiceClient = new BlobServiceClient(sasUri)
+      const containerClient = blobServiceClient.getContainerClient(containerName)
+      const client = containerClient.getBlockBlobClient(filedId)
+      const response = await client.uploadData(this.currentFile!, { 
         blockSize: 3 * 1024 * 1024 * 1024, // 3GB block size
         concurrency: 20, // 20 concurrency
-        onProgress: (ev) => console.log(ev),
-        blobHTTPHeaders :{ blobContentType:this.currentFile!.type}
+        onProgress: (ev) => console.log(ev.loadedBytes),
+        blobHTTPHeaders :{ 
+          blobContentType: 'application/octet-stream',
+          blobContentDisposition: 'form-data'
+        }
       })
 
       console.log(response._response.status)
